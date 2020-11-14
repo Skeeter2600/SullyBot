220ba5da3f8adf3f5314a182efdcb3a186eeb804
Joshua McEntire <jsm31940@gmail.com>
2020-11-14 01:33:20 -0500

    Commented Youtube workings and such

--- Bot/bot.py
+++ Bot/bot.py
@@ -3,7 +3,7 @@ import asyncio
 import discord
 import os
 import random
-import youtube_dl
+# import youtube_dl
 from discord.ext import commands
 
 TOKEN = "NzExNjc2MzE5NjcwNDY4NzQw.XsGeOA.HTvkWb2n4QEZtr9XFKJ85227vpM"
@@ -13,52 +13,52 @@ smashers = []
 smash_queue = []
 smash_queue_pointer = 0
 
-ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+# ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
-youtube_dl.utils.bug_reports_message = lambda: ''
-# 🥺🥺🥺🥺🥺🥺
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-#test
-#hi josh
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+# youtube_dl.utils.bug_reports_message = lambda: ''
+# # 🥺🥺🥺🥺🥺🥺
+# ytdl_format_options = {
+#     'format': 'bestaudio/best',
+#     'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+#     'restrictfilenames': True,
+#     'noplaylist': True,
+#     'nocheckcertificate': True,
+#     'ignoreerrors': False,
+#     'logtostderr': False,
+#     'quiet': True,
+#     'no_warnings': True,
+#     'default_search': 'auto',
+#     'source_address': '0.0.0.0'  # bind to ipv4 since ipv6 addresses cause issues sometimes
+# }
 
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
+# ffmpeg_options = {
+#     'options': '-vn'
+# }
 
-        self.data = data
+# ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
 
-        self.title = data.get('title')
-        self.url = data.get('url')
 
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+# class YTDLSource(discord.PCMVolumeTransformer):
+#     def __init__(self, source, *, data, volume=0.5):
+#         super().__init__(source, volume)
 
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
+#         self.data = data
 
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
+#         self.title = data.get('title')
+#         self.url = data.get('url')
+
+#     @classmethod
+#     async def from_url(cls, url, *, loop=None, stream=False):
+#         loop = loop or asyncio.get_event_loop()
+#         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+#         if 'entries' in data:
+#             # take first item from a playlist
+#             data = data['entries'][0]
+
+#         filename = data['url'] if stream else ytdl.prepare_filename(data)
+#         return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 @client.event
